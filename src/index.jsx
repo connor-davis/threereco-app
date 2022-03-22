d0e4ce85b6b99626c57bca1fa5992ae14db291a1
Connor Davis <19422@kloofhigh.co.za>
2022-03-22 12:15:20 +0200

    Get the app to be a pwa

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,29 @@
-import { render } from 'solid-js/web';
-
-import './index.css';
-import App from './App';
 import { Router } from 'solid-app-router';
+import { render } from 'solid-js/web';
+import { registerSW } from 'virtual:pwa-register';
+import App from './App';
+import './index.css';
 
-let Routed = () => {
-    return (
-        <Router>
-            <App />
-        </Router>
-    )
+
+if (typeof global === 'undefined') {
+  window.global = window;
 }
 
+let Routed = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
 render(Routed, document.getElementById('root'));
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
+
+if (typeof window !== 'undefined') {
+  import('./sw');
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,88 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      '@src': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      srcDir: 'src',
+      base: '/',
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+      },
+      id: '/',
+      includeAssets: [
+        './src/robots.txt',
+        './src/assets/3rEco-x16.png',
+        './src/assets/3rEco-x32.png',
+        './src/assets/3rEco-x64.png',
+        './src/assets/3rEco-x128.png',
+        './src/assets/3rEco-x144.png',
+        './src/assets/3rEco-x256.png',
+        './src/assets/3rEco-x512.png',
+      ],
+      strategies: 'injectManifest',
+      manifest: {
+        short_name: '3rEco',
+        name: '3rEco | Growing Together.',
+        icons: [
+          {
+            src: 'assets/3rEco-x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'assets/3rEco-x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'assets/3rEco-x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'assets/3rEco-x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'assets/3rEco-x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'assets/3rEco-x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'assets/3rEco-x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
